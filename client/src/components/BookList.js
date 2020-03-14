8e2120564a624f1a139da8e2b53b10f991032075
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-14 04:23:37 +0330

    load books in BookList

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
 
 // load queries
 import { getBooksQuery } from "./../queries/queries";
 
 const BookList = props => {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  const getBooks = data => {
+    return data.books.map(book => {
+      return <li key={book.id}>{book.name}</li>;
+    });
+  };
+
   let content = (
     <div>
-      <ul id="book-list"></ul>
+      <ul id="book-list">{getBooks(data)}</ul>
     </div>
   );
 
